2d30fed7b8c5131331e77a42400c3dbac7c6d4ca
forest-lynx <trot505@yandex.ru>
2024-08-21 12:34:41 +0400

    Изменено отображение поля, для корректного отображения переписано на flex. Добавлен корректный вывод ошибок при групповом поле.

--- resources/views/fields/decimal.blade.php
+++ resources/views/fields/decimal.blade.php
@@ -1,33 +1,44 @@
-<x-moonshine::grid class="decimal-field">
-    <x-moonshine::column adaptiveColSpan="6" colSpan="{{ $element->isGroup() ? '8' : '12' }}">
-        <x-moonshine::form.input-extensions
-                    :extensions="$element->getExtensions()"
-            >
-            <x-moonshine::form.input
-                :attributes="$element->attributes()->merge([
-                    'id' => $element->id(),
-                    'name' => $element->name($element->column()),
-                    'value' => (string) $value
-                ])"
-                @class(['form-invalid' => formErrors($errors, $element->getFormName())->has($element->name($element->column()))])
-            />
-        </x-moonshine::form.input-extensions>
-    </x-moonshine::column>
+<div class="decimal-field {{$element->isGroup() ? 'decimal-field-group': ''}}">
+
+    <x-moonshine::form.input-extensions
+        :extensions="$element->getExtensions()"
+    >
+        <x-moonshine::form.input
+            :attributes="$element->attributes()->merge([
+                'id' => $element->id(),
+                'name' => $element->name($element->column()),
+                'value' => (string) (is_array($value) ? $value[$element->column()] : $value)
+            ])"
+            @class(['form-invalid' => formErrors($errors, $element->getFormName())->has($element->column())])
+        />
+    </x-moonshine::form.input-extensions>
     @if($element->isGroup())
-    <x-moonshine::column adaptiveColSpan="6" colSpan="4">
-            <x-moonshine::form.select
-                :id="$element->getUnitColumn()"
-                :name="$element->name($element->getUnitColumn())"
-                data-item-select-text=""
-            >
-                <x-slot:options>
-                    @foreach($element->values() as $key => $unit)
-                        <option value="{{ $key }}"
-                        @if($element->isSelected($key)) selected @endif
-                                >{{ $unit }}</option>
-                    @endforeach
-                </x-slot:options>
-            </x-moonshine::form.select>
-    </x-moonshine::column>
+        <x-moonshine::form.select
+            :id="$element->getUnitColumn()"
+            :name="$element->name($element->getUnitColumn())"
+            data-item-select-text=""
+
+            @class(['form-invalid' => formErrors($errors, $element->getFormName())->has($element->getUnitColumn())])
+        >
+            <x-slot:options>
+                @foreach($element->values() as $key => $unit)
+                    <option value="{{ $key }}"
+                    @if($element->isSelected($key)) selected @endif
+                            >{{ $unit }}</option>
+                @endforeach
+            </x-slot:options>
+        </x-moonshine::form.select>
     @endif
-</x-moonshine::grid>
+</div>
+@if($element->isGroup())
+    @error($element->column(), $element->getFormName())
+        <x-moonshine::form.input-error>
+            {{ $message }}
+        </x-moonshine::form.input-error>
+    @enderror
+    @error($element->getUnitColumn(), $element->getFormName())
+        <x-moonshine::form.input-error>
+            {{ $message }}
+        </x-moonshine::form.input-error>
+    @enderror
+@endif
